3bab30238fd94e6a20e5e1ad7652557177d18496
Manikandan M <143875381+Manikandan-8078@users.noreply.github.com>
2025-11-16 16:31:14 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,25 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
